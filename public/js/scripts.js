dae56acd4517943224ce095884653a14ba8be14d
AksshatGupta <aksshatyashgupta@gmail.com>
2018-12-23 12:14:42 -0500

    added cookie functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,51 @@
-alert("Hello World")
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
+
+function deleteCookie(cname) {
+  document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+$(function() {
+  $("#render-username").html(getCookie("username"));
+  $("#check").on("click", function() {
+    var user = $("#username").val();
+    setCookie("username", user, 365);
+    $("#render-username").html(getCookie("username"));
+  })
+  $("#delete").on("click", function() {
+    var cname = $(this).data("cookie");
+    deleteCookie(cname);
+    $("#render-username").html(getCookie("username"));
+  })
+});
